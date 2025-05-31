106916f60731c0546b6a23e55983ef65922ff44d
chucklestephan <123928522+chucklestephan@users.noreply.github.com>
2025-05-31 14:04:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 (function() {
 
   const faders = document.querySelectorAll('.fade-in')
+  const sliders = document.querySelectorAll('.slide-in')
   window.addEventListener('load', init);
   let currentPageNumber = -1;
   let book = 'homecoming';
@@ -71,8 +72,8 @@
 })();
 
 const appearOptions = {
-  threshold: 1,
-  rootMargin: "0px 0px -100px 0px"
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
 };
 const appearOnScroll = new IntersectionObserver 
   (function(entries, appearOnScroll) { entries.forEach(entry => {
@@ -89,3 +90,7 @@ appearOptions);
 faders.forEach(fader => {
   appearOnScroll.observe(fader);
 });
+
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
