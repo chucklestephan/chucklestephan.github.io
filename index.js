d2d2e549092365eba841279788b9a40613561f03
chucklestephan <123928522+chucklestephan@users.noreply.github.com>
2025-06-07 23:53:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 (function() {
 
-  const faders = document.querySelectorAll('.fade-in')
-  const sliders = document.querySelectorAll('.slide-in')
   window.addEventListener('load', init);
   let currentPageNumber = -1;
   let book = 'homecoming';
@@ -71,26 +69,3 @@
   }
 })();
 
-const appearOptions = {
-  threshold: 0,
-  rootMargin: "0px 0px -250px 0px"
-};
-const appearOnScroll = new IntersectionObserver 
-  (function(entries, appearOnScroll) { entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      entry.target.classlist.add('appear');
-      appearOnScroll.unobserve(entry.target);
-    }
-  });
-                                     },
-appearOptions);
-
-faders.forEach(fader => {
-  appearOnScroll.observe(fader);
-});
-
-
-sliders.forEach(slider => {
-  appearOnScroll.observe(slider);
